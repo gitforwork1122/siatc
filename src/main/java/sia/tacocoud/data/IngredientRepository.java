524b4dc663aa0ebd8ceb108e36d82401686ee354
Costi <test@test.com>
2024-05-28 08:53:14 +0300

    Fixed repos

--- src/main/java/sia/tacocoud/data/IngredientRepository.java
+++ src/main/java/sia/tacocoud/data/IngredientRepository.java
@@ -1,10 +1,8 @@
 package sia.tacocoud.data;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.data.jpa.repository.JpaRepository;
 import sia.tacocoud.model.Ingredient;
 
-@CrossOrigin(origins="http://localhost:8080")
-public interface IngredientRepository extends CrudRepository<Ingredient, String> {
+
+public interface IngredientRepository extends JpaRepository<Ingredient, String> {
 }
--- src/main/java/sia/tacocoud/data/OrderRepository.java
+++ src/main/java/sia/tacocoud/data/OrderRepository.java
@@ -1,14 +1,13 @@
 package sia.tacocoud.data;
 
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import sia.tacocoud.model.Order;
 import sia.tacocoud.model.User;
 import java.util.List;
 
 
-public interface OrderRepository extends CrudRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findByUserOrderByPlacedAtDesc(User user);
 

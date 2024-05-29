2ec94880e605efe721caec4192b774c75557ec65
Costi <test@test.com>
2024-05-29 10:04:13 +0300

    For testing delete

---
+++ src/main/java/sia/tacocoud/messaging/KafkaOrderMessagingService.java
@@ -0,0 +1,23 @@
+package sia.tacocoud.messaging;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import sia.tacocoud.model.Order;
+
+@Service
+public class KafkaOrderMessagingService implements OrderMessagingService{
+
+    private KafkaTemplate<String, Order> kafkaTemplate;
+
+    @Autowired
+    public KafkaOrderMessagingService(KafkaTemplate<String, Order> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Override
+    public void sendOrder(Order order) {
+        kafkaTemplate.sendDefault(order);
+    }
+
+}
